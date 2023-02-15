19ccc3e17f5f4cad33387c7a24d81f9278c2a66c
TeeZ21 <54781489+TeeZ21@users.noreply.github.com>
2023-02-15 16:11:44 +0100

    Fix BlackFade

--- Assets/Scripts/UI/LevelChanger.cs
+++ Assets/Scripts/UI/LevelChanger.cs
@@ -7,7 +7,6 @@ public class LevelChanger : MonoBehaviour
 {
     public Animator _animator = null;
     private int _levelToLoad;
-
     void Start()
     {
 
@@ -15,20 +14,32 @@ public class LevelChanger : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             FadeToLevel(1);
-        }
+        }*/
     }
-
     public void FadeToLevel(int levelIndex)
     {
         _levelToLoad = levelIndex;
         _animator.SetTrigger("FadeOut");
     }
 
+    public void FadeToLevels()
+    {
+
+        _animator.SetTrigger("FadeOut");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void OnFadeComplete()
     {
-        SceneManager.LoadScene(_levelToLoad);
+        SceneManager.LoadScene("GameScene");
+        //SceneManager.LoadScene(_levelToLoad);
     }
 }
