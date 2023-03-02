9070563f6883b9f6d38c77c0fefa8e1603560e6b
TeeZ21 <54781489+TeeZ21@users.noreply.github.com>
2023-03-02 12:00:31 +0100

    Add Earth

---
+++ Assets/Scripts/EarthSpin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpin : MonoBehaviour {
+    [SerializeField] private float speed = 10f;
+
+    void Update() {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+    }
+}
--- Assets/Scripts/MouseRotation.cs
+++ Assets/Scripts/MouseRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class MouseRotation : MonoBehaviour
 {
     [SerializeField] private float _rotSpeed = 20;
 
@@ -11,10 +11,13 @@ public class Rotation : MonoBehaviour
     {
         
     }
+
+    [System.Obsolete]
     void OnMouseDrag()
     {
         float rotX = Input.GetAxis("Mouse X") * _rotSpeed * Mathf.Deg2Rad;
 
-        transform.RotateAround(Vector3.up, -rotX);
+        transform.Rotate(Vector3.up, -rotX);
+        //transform.RotateAround(Vector3.up, -rotX);
     }
 }
