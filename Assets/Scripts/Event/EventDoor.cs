aa7e218b348e175a89de97eb6c0f66af48d736b0
Hadrien <101469529+Hadrideux@users.noreply.github.com>
2023-04-18 10:25:46 +0200

    [Correction Ortographe]

--- Assets/Scripts/Event/EventDoor.cs
+++ Assets/Scripts/Event/EventDoor.cs
@@ -34,6 +34,7 @@ public class EventDoor : MonoBehaviour
         get => _leakValue; 
         set => _leakValue = value;
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -58,7 +59,7 @@ public class EventDoor : MonoBehaviour
         }
     }
 
-    // soustraint une partie du cache du styx et les envoi vers le tartare
+    // soustrait une partie du cache du styx et les envoi vers le tartare
     public void LeakEvent()
     {
         if (_leakCycleDuration == 1)
